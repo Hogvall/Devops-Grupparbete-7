2d80cee4557a030c77d2582a427aa49c9b1b958a
Sofie Johnsson <a.e.sofie.o@gmail.com>
2026-04-05 22:20:23 +0200

    Comments and cleanup

--- scripts/meeting.js
+++ scripts/meeting.js
@@ -1,122 +1,128 @@
+//Mocking a logged in user.
 const currentUser = "b2b2b2b2-b2b2-b2b2-b2b2-b2b2b2b2b2b2";
 
-export async function getMeeting(id) {
-  try {
-    const response = await fetch(`https://wufgfgadhvprxdkcducx.supabase.co/rest/v1/meetings?apikey=sb_publishable_RQEs3xlyjHfIs2X3Ql6jbQ_KsKkQZze&select=*,category(name)&id=eq.${id}`);
+//Subabase variables
+const supabase_token = "sb_publishable_RQEs3xlyjHfIs2X3Ql6jbQ_KsKkQZze";
+const supabase_baseurl = "https://wufgfgadhvprxdkcducx.supabase.co";
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+//Fetching a meeting by id.
+export async function getMeeting(meetingId) {
+    try {
+        const response = await fetch(`${supabase_baseurl}/rest/v1/meetings?apikey=${supabase_token}&select=*,category(name)&id=eq.${meetingId}`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const meeting = await response.json();
+        return meeting;
+
+    } catch (error) {
+        console.error('Fetch error:', error);
     }
-
-    const meeting = await response.json();
-
-    return meeting;
-
-  } catch (error) {
-    console.error('Fetch error:', error);
-  }
 }
 
-export async function getParticipantData(id) {
-  try {
-    const response = await fetch(`https://wufgfgadhvprxdkcducx.supabase.co/rest/v1/meeting_participant?apikey=sb_publishable_RQEs3xlyjHfIs2X3Ql6jbQ_KsKkQZze&meeting_id=eq.${id}`);
+//Fetching users that signed up for the meeting.
+export async function getParticipantData(meetingId) {
+    try {
+        const response = await fetch(`${supabase_baseurl}/rest/v1/meeting_participant?apikey=${supabase_token}&meeting_id=eq.${meetingId}`);
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const participants = await response.json();
+        return participants;
+
+    } catch (error) {
+        console.error('Fetch error:', error);
     }
-
-    const participants = await response.json();
-
-    return participants;
-
-  } catch (error) {
-    console.error('Fetch error:', error);
-  }
 }
 
+//Signing up user to meeting
 export async function addParticipant(userId, meetingId) {
-  const response = await fetch(`https://wufgfgadhvprxdkcducx.supabase.co/rest/v1/meeting_participant`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "apikey": "sb_publishable_RQEs3xlyjHfIs2X3Ql6jbQ_KsKkQZze",
-      "Authorization": `Bearer sb_publishable_RQEs3xlyjHfIs2X3Ql6jbQ_KsKkQZze`,
-      "Prefer": "return=representation"
-    },
-    body: JSON.stringify({
-      user_id: userId,
-      meeting_id: meetingId
-    })
-  });
+    const response = await fetch(`${supabase_baseurl}/rest/v1/meeting_participant`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "apikey": `${supabase_token}`,
+            "Authorization": `Bearer ${supabase_token}`,
+            "Prefer": "return=representation"
+        },
+        body: JSON.stringify({
+            user_id: userId,
+            meeting_id: meetingId
+        })
+    });
 
-  const data = await response.json();
-  renderParticipantDiv(meetingId);
-  return data;
+    renderParticipantDiv(meetingId);
+
+    const data = await response.json();
+    return data;
 }
 
+//Removing user from meeting
 export async function deleteParticipant(userId, meetingId) {
-  const url = `https://wufgfgadhvprxdkcducx.supabase.co/rest/v1/meeting_participant?user_id=eq.${userId}&meeting_id=eq.${meetingId}`;
+    const url = `${supabase_baseurl}/rest/v1/meeting_participant?user_id=eq.${userId}&meeting_id=eq.${meetingId}`;
 
-  const response = await fetch(url, {
-    method: "DELETE",
-    headers: {
-      "apikey": "sb_publishable_RQEs3xlyjHfIs2X3Ql6jbQ_KsKkQZze",
-      "Authorization": "Bearer sb_publishable_RQEs3xlyjHfIs2X3Ql6jbQ_KsKkQZze"
-    }
-  });
+    const response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+            "apikey": `${supabase_token}`,
+            "Authorization": `Bearer ${supabase_token}`,
+        }
+    });
 
-  const data = await response.json().catch(() => null);
-  renderParticipantDiv(meetingId);
+    renderParticipantDiv(meetingId);
 
-  return data;
+    const data = await response.json().catch(() => null);
+    return data;
 }
 
-export async function renderParticipantDiv(id) {
+//Render no of sign-ups and button for signing up/cancelling.
+export async function renderParticipantDiv(meetingId) {
 
-    const participants = await getParticipantData(id);
+    const participants = await getParticipantData(meetingId);
     const participantDiv = document.getElementById('participantDiv');
     const button = document.createElement('button');
 
-    participantDiv.innerHTML = "Anmälda: " + participants.length;
+    participantDiv.innerHTML = `<div>Anmälda: ${participants.length}</div>`;
 
     let isSignedUp;
-    if(participants.length > 0){
+    if (participants.length > 0) {
 
-        for(let participant of participants){
-            if(participant.user_id == currentUser){
+        for (let participant of participants) {
+            if (participant.user_id == currentUser) {
                 isSignedUp = true;
                 break;
+            }else{
+                isSignedUp = false;
             }
         }
     }
 
-    if(isSignedUp == true){
-
+    if (isSignedUp == true) {
         button.textContent = "Avanmäl här";
-        button.addEventListener("click", () => deleteParticipant(currentUser, id));
-
-    }
-    else{
-        
+        button.addEventListener("click", () => deleteParticipant(currentUser, meetingId));
+    }else{
         button.textContent = "Anmäl här";
-        button.addEventListener("click", () => addParticipant(currentUser, id));
-
+        button.addEventListener("click", () => addParticipant(currentUser, meetingId));
     }
 
     participantDiv.append(button);
-    
 
 }
 
+//Render meeting and related data
 export async function renderMeeting() {
 
     const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get('id');
+    const meetingId = urlParams.get('id');
 
-    const meeting = await getMeeting(id);
+    const meeting = await getMeeting(meetingId);
 
     console.log(meeting);
-    
+
     const main = document.getElementById('app');
     const img = document.createElement('img');
     const category = document.createElement('div');
@@ -130,9 +136,7 @@ export async function renderMeeting() {
     const formattedDate = new Intl.DateTimeFormat("en-en", {
         dateStyle: "long",
         timeStyle: "short"
-        }).format(new Date(meeting[0].time));
-
-        console.log(formattedDate);
+    }).format(new Date(meeting[0].time));
 
     img.src = meeting[0].image;
     category.textContent = "Category: " + meeting[0].category.name
@@ -143,7 +147,7 @@ export async function renderMeeting() {
 
     main.append(img, title, timeloc, description, category, participantDiv);
 
-    renderParticipantDiv(id);
+    renderParticipantDiv(meetingId);
 }
 
 renderMeeting();
