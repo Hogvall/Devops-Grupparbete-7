64fe141873912ecfad1899af45dbaa065e5af3f6
Sofie Johnsson <a.e.sofie.o@gmail.com>
2026-04-05 22:20:50 +0200

    Test files added

---
+++ tests/integration/meeting.test.js
@@ -0,0 +1,13 @@
+import { describe, test, expect, beforeEach } from "vitest";
+
+describe("Description", () => {
+  
+  beforeEach(() => {
+   
+  });
+
+  test("Test description", () => {
+    expect(1).toEqual(1);
+  });
+  
+});
