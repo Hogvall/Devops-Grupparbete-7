c1c94d7cfbc1461e3742c93463230f07857558ad
Sofie Johnsson <a.e.sofie.o@gmail.com>
2026-04-07 13:04:12 +0200

    Simplel e2e test

--- playwright.config.js
+++ playwright.config.js
@@ -1,15 +1,15 @@
-/* eslint-disable no-undef */
-
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests/e2e",
-  webServer: {
-    command: "npm run preview",
-    port: 4173,
-    reuseExistingServer: !process.env.CI,
-  },
+
   use: {
-    baseURL: "http://localhost:4173",
+    baseURL: "http://localhost:5173", // eller 4173 om du kör preview
+  },
+
+  webServer: {
+    command: "npm run dev",
+    port: 5173,
+    reuseExistingServer: !process.env.CI,
   },
 });
--- tests/e2e/meeting.test.js
+++ tests/e2e/meeting.test.js
@@ -1,6 +1,9 @@
 import { test, expect } from "@playwright/test";
 
 test("Test description", async ({ page }) => {
-  await page.goto("/");
-  expect(1).toEqual(1);
+  await page.goto("http://localhost:5173/meeting.html?id=2");
+
+  await page.waitForSelector("h3");
+  await expect(page.locator("h3").first()).not.toBeEmpty;
+
 });
