fcab3b78e83bf2791bd807a173da6dd3b2f54ad1
Sofie Johnsson <a.e.sofie.o@gmail.com>
2026-04-07 16:09:35 +0200

    Add await

--- tests/e2e/meeting.test.js
+++ tests/e2e/meeting.test.js
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('h3-elementet laddas', async ({ page }) => {
   await page.goto('/meeting.html?id=2'); // eller den sida du testar
-  const h3 = page.locator('h3');
+  const h3 = await page.locator('h3');
   await expect(h3).toBeVisible();
 });
