39cf9bc44345624d5ac3c93227aa666fc4e4f191
shakur <shakur.mohamoud@gmail.com>
2026-04-08 20:13:45 +0200

    fixed e2e test

--- tests/e2e/search.test.js
+++ tests/e2e/search.test.js
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("should display search results", async ({ page }) => {
-  await page.goto("http://localhost:5173");
+  await page.goto("/");
 
   await page.selectOption("#category", "");
   await page.selectOption("#location", "");
