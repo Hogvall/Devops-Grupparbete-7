db7918c8230d1bbe762f0626ad3f04876f40bc5c
Sofie Johnsson <a.e.sofie.o@gmail.com>
2026-04-08 11:41:20 +0200

    Basic CSS added

--- tests/e2e/meeting.test.js
+++ tests/e2e/meeting.test.js
@@ -11,11 +11,11 @@ test("User can sign up and unsign using API", async ({ page }) => {
 
   // Wait for text-content
   const countLocator = page.locator("#participantDiv");
-  await expect(countLocator).toContainText("Anmälda:");
+  await expect(countLocator).toContainText("Registered participants:");
 
   // Find button
   const signUpButton = page.locator('#participantDiv button');
-  await expect(signUpButton).toHaveText("Anmäl här");
+  await expect(signUpButton).toHaveText("Sign up");
 
   const initialText = await countLocator.innerText();
   const initialCount = Number(initialText.match(/\d+/)[0]);
@@ -24,14 +24,14 @@ test("User can sign up and unsign using API", async ({ page }) => {
   await signUpButton.click();
 
   // Wait for DOM to update
-  await expect(countLocator).toContainText(`Anmälda:`);
-  await expect(countLocator).toContainText("Anmälda: " + (initialCount + 1) + " /");
+  await expect(countLocator).toContainText(`Registered participants:`);
+  await expect(countLocator).toContainText("Registered participants: " + (initialCount + 1) + " /");
 
   // Click again
   const updatedButton = page.locator('#participantDiv button');
-  await expect(updatedButton).toHaveText("Avanmäl här");
+  await expect(updatedButton).toHaveText("Cancel");
   await updatedButton.click();
 
   // Wait for count to change to initial value
-  await expect(page.locator('#participantDiv')).toContainText(`Anmälda: ${initialCount}`);
+  await expect(page.locator('#participantDiv')).toContainText(`Registered participants: ${initialCount}`);
 });
