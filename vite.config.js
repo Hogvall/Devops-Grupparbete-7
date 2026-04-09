8cbb21a8639e382bb516b5ce447c98d5fd8ea7b7
Marcus Högvall <marhog9706@gmail.com>
2026-04-09 10:59:11 +0200

    Fix vite config resolve paths

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,8 @@ export default defineConfig({
         meeting: resolve(__dirname, "meeting.html"),
         createMeeting: resolve(__dirname, "create-meeting.html"),
         profile: resolve(__dirname, "profile.html"),
-        login: 'login.html',      
-        calendar: 'calendar.html' 
+        login: resolve(__dirname, "login.html"),
+        calendar: resolve(__dirname, "calendar.html")
       },
     },
   },
